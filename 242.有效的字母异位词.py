a4ecf31c0bf88c658a29f9f829348bef2092df78
liub09 <liub09@mingyuanyun.com>
2020-06-18 09:05:47 +0800

    618

---
+++ 242.有效的字母异位词.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode.cn id=242 lang=python3
+#
+# [242] 有效的字母异位词
+#
+# https://leetcode-cn.com/problems/valid-anagram/description/
+#
+# algorithms
+# Easy (60.18%)
+# Likes:    204
+# Dislikes: 0
+# Total Accepted:    107.9K
+# Total Submissions: 179.2K
+# Testcase Example:  '"anagram"\n"nagaram"'
+#
+# 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+# 
+# 示例 1:
+# 
+# 输入: s = "anagram", t = "nagaram"
+# 输出: true
+# 
+# 
+# 示例 2:
+# 
+# 输入: s = "rat", t = "car"
+# 输出: false
+# 
+# 说明:
+# 你可以假设字符串只包含小写字母。
+# 
+# 进阶:
+# 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        cm = {}
+        for c in s:
+            cm[c] = (0 if c not in cm else cm[c]) + 1
+
+        for c in t:
+            cm[c] = (0 if c not in cm else cm[c]) - 1
+            if cm[c] < 0:
+                return False
+        
+        return True
+
+
+# @lc code=end
+
